26a54ade40b44f36a188842978dae5c29654caf2
brianbohanon <brian.bohanon@gmail.com>
2014-09-12 13:38:02 -0400

    First iteration of calculator.windowsform

---
+++ Calendar.WindowsForm/Calendar.WindowsForm/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calendar.WindowsForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double val1, val2;
+            val1 = double.Parse(textBox1.Text);
+            val2 = double.Parse(textBox2.Text);
+            textBox3.Text = (val1 + val2).ToString();
+        }
+    }
+}
