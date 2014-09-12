a87669bcb16656bde3bd64954956fb79581fa95b
brianbohanon <brian.bohanon@gmail.com>
2014-09-12 15:57:18 -0400

    Added buttons and combo box

--- Calendar.WindowsForm/Calendar.WindowsForm/Form1.Designer.cs
+++ Calendar.WindowsForm/Calendar.WindowsForm/Form1.Designer.cs
@@ -33,6 +33,9 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // textBox1
@@ -69,7 +72,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(172, 119);
+            this.button2.Location = new System.Drawing.Point(71, 90);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 23);
             this.button2.TabIndex = 1;
@@ -77,16 +80,53 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(172, 152);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Form 2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(145, 119);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(127, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Add Button Enabled?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Add",
+            "Multiply"});
+            this.comboBox1.Location = new System.Drawing.Point(71, 193);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(201, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 173);
+            this.ClientSize = new System.Drawing.Size(284, 226);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -101,6 +141,9 @@
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- Calendar.WindowsForm/Calendar.WindowsForm/Form1.cs
+++ Calendar.WindowsForm/Calendar.WindowsForm/Form1.cs
@@ -24,6 +24,14 @@ namespace Calculator.WindowsForm
             double val1 = double.Parse(textBox1.Text);
             double val2 = double.Parse(textBox2.Text);
             textBox3.Text = Calc.AddNumbers(val1, val2).ToString();
+            if (comboBox1.Text.Equals("Add"))
+            {
+                textBox3.Text = Calc.AddNumbers(val1, val2).ToString();
+            }
+            else
+            {
+                textBox3.Text = Calc.MultiplyNumbers(val1, val2).ToString();
+            }
         }        
 
         private void button2_Click(object sender, EventArgs e)
@@ -31,6 +39,24 @@ namespace Calculator.WindowsForm
             double val1 = double.Parse(textBox1.Text);
             double val2 = double.Parse(textBox2.Text);
             textBox3.Text = Calc.MultiplyNumbers(val1, val2).ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 NewForm = new Form2();
+            NewForm.Show();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked == true)
+            {
+                button1.Enabled = true;
+            }
+            else
+            {
+                button1.Enabled = false;
+            }
         }
     }
 }
